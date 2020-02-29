48bdd217ae731fb07280956780378dbb4741e9c2
DragonV96 <510537547@qq.com>
2020-02-29 17:26:04 +0800

    [init]Initial commit

---
+++ ad-eureka/src/main/java/com/glw/ad/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.glw.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author : glw
+ * @date : 2020/2/29
+ * @time : 16:36
+ * @Description :
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
