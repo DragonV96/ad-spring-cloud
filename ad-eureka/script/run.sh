509cc20df0734f8b268088072ef6fe1a10f57859
DragonV96 <510537547@qq.com>
2020-03-01 11:32:18 +0800

    [add]eureka切换为集群部署模式

---
+++ ad-eureka/script/run.sh
@@ -0,0 +1,12 @@
+cd ad-eureka
+cd target
+java -jar ad-eureka-0.0.1-SNAPSHOT.jar --spring.profiles.active=server1
+
+cd ad-eureka
+cd target
+java -jar ad-eureka-0.0.1-SNAPSHOT.jar --spring.profiles.active=server2
+
+cd ad-eureka
+cd target
+java -jar ad-eureka-0.0.1-SNAPSHOT.jar --spring.profiles.active=server3
+
