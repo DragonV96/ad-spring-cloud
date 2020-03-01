390b7e3dbf876dde929fdd9482dd34a211e7dced
DragonV96 <510537547@qq.com>
2020-03-01 12:59:51 +0800

    [add]添加zuul网关，eureka切换为单机部署模式

---
+++ ad-gateway/src/main/java/com/glw/ad/ZuulGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.glw.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author : glw
+ * @date : 2020/3/1
+ * @time : 11:44
+ * @Description : 网关启动类
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+}
---
+++ ad-gateway/src/main/java/com/glw/ad/common/Constants.java
@@ -0,0 +1,13 @@
+package com.glw.ad.common;
+
+/**
+ * @author : glw
+ * @date : 2020/3/1
+ * @time : 11:58
+ * @Description : 常量类
+ */
+public class Constants {
+
+    public static final  String START_TIME = "startTime";
+
+}
