f9cf6f677e4eddc0701a44fe88474a702e3d6f8c
DragonV96 <510537547@qq.com>
2020-03-04 00:27:49 +0800

    [add]添加广告投放系统初始模块及相关配置

---
+++ ad-service/ad-sponsor/src/main/java/com/glw/ad/SponsorApplication.java
@@ -0,0 +1,23 @@
+package com.glw.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author : glw
+ * @date : 2020/3/3
+ * @time : 23:29
+ * @Description : 广告投放系统启动类
+ */
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableEurekaClient
+@SpringBootApplication
+public class SponsorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SponsorApplication.class, args);
+    }
+}
