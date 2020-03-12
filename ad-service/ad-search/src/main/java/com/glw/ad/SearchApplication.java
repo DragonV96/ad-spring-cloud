eeaeea0efb76023d8be1928f472916fcf1cce835
DragonV96 <510537547@qq.com>
2020-03-13 00:20:52 +0800

    [add]添加广告搜索系统

---
+++ ad-service/ad-search/src/main/java/com/glw/ad/SearchApplication.java
@@ -0,0 +1,30 @@
+package com.glw.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author : glw
+ * @date : 2020/3/12
+ * @time : 23:24
+ * @Description : 搜索启动类
+ */
+@EnableFeignClients
+@EnableEurekaClient
+@EnableHystrix
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@EnableHystrixDashboard
+@SpringBootApplication
+public class SearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchApplication.class);
+    }
+}
