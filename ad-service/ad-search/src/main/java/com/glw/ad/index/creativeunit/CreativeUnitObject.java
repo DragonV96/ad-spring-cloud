748dbea54c094b1e0ebe602c2dbf9dd910175a46
DragonV96 <510537547@qq.com>
2020-03-14 11:17:42 +0800

    [add]广告检索系统添加推广单元地域，创意与推广单元的关联，推广单元创意等索引业务逻辑实现

---
+++ ad-service/ad-search/src/main/java/com/glw/ad/index/creativeunit/CreativeUnitObject.java
@@ -0,0 +1,21 @@
+package com.glw.ad.index.creativeunit;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author : glw
+ * @date : 2020/3/14
+ * @time : 10:57
+ * @Description : 创意与推广单元实体对象
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreativeUnitObject {
+
+    private Long adId;
+
+    private Long unitId;
+}
---
+++ ad-service/ad-search/src/main/java/com/glw/ad/index/district/UnitDistrictObject.java
@@ -0,0 +1,16 @@
+package com.glw.ad.index.district;
+
+/**
+ * @author : glw
+ * @date : 2020/3/14
+ * @time : 10:59
+ * @Description : 推广单元地域实体对象（省份和城市合并为 province-city 存储）
+ */
+public class UnitDistrictObject {
+
+    private Long unitId;
+
+    private String province;
+
+    private String city;
+}
